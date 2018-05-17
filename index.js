8117f8ece08938fff289874b3f30f24bde2eabb4
Sarah Dilworth <sarah.petty18@gmail.com>
2018-05-17 00:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var a = Math.floor(Math.random() *1000)
 var b = Math.floor(Math.random() *1000)
 function add() {
-  return (a += b)
+a += b
 }
 var a =  Math.floor(Math.random() *1000)
 var b =  Math.floor(Math.random() *1000)
 function subtract() {
-  return a -= b
+ a -= b
 }
 var a =  Math.floor(Math.random() *1000)
 var b =  Math.floor(Math.random() *1000)
 function multiply() {
-  return a *= b
+ a *= b
 }
 var a =  Math.floor(Math.random() *1000)
 var b =  Math.floor(Math.random() *1000)
 function divide() {
-  return a /= b
+ a /= b
 }
